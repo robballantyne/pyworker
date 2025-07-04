9136e414b3685f530e844c953b1fcdbea85a52d4
Rob Ballantyne <rob@robballantyne.co.uk>
2025-07-04 18:41:02 +0100

    Basic client example

--- workers/vllm/client.py
+++ workers/vllm/client.py
@@ -13,8 +13,8 @@ logging.basicConfig(
 log = logging.getLogger(__file__)
 
 
-def call_generate(endpoint_group_name: str, api_key: str, server_url: str) -> None:
-    WORKER_ENDPOINT = "/generate"
+def call_generic_completions(endpoint_group_name: str, api_key: str, server_url: str) -> None:
+    WORKER_ENDPOINT = "/generic"
     COST = 100
     route_payload = {
         "endpoint": endpoint_group_name,
@@ -38,7 +38,63 @@ def call_generate(endpoint_group_name: str, api_key: str, server_url: str) -> No
         url=url,
     )
 
-    payload = dict(inputs="tell me about cats", parameters=dict(max_new_tokens=500))
+    payload = dict(
+        input=dict(
+            model="deepseek-ai/DeepSeek-R1-Distill-Llama-8B",
+            prompt="The capital of France is",
+            max_tokens=10,
+            temperature=0
+        ),
+        endpoint="/v1/completions",
+        method="POST"
+    )
+
+    req_data = dict(payload=payload, auth_data=auth_data)
+    url = urljoin(url, WORKER_ENDPOINT)
+    print(f"url: {url}")
+    response = requests.post(url, json=req_data)
+    response.raise_for_status()
+    res = response.json()
+    print(res)
+
+def call_generic_chat_completions(endpoint_group_name: str, api_key: str, server_url: str) -> None:
+    WORKER_ENDPOINT = "/generic"
+    COST = 100
+    route_payload = {
+        "endpoint": endpoint_group_name,
+        "api_key": api_key,
+        "cost": COST,
+    }
+    response = requests.post(
+        urljoin(server_url, "/route/"),
+        json=route_payload,
+        timeout=4,
+    )
+    response.raise_for_status()  # Raise an exception for bad status codes
+    message = response.json()
+    url = message["url"]
+
+    auth_data = dict(
+        signature=message["signature"],
+        cost=message["cost"],
+        endpoint=message["endpoint"],
+        reqnum=message["reqnum"],
+        url=url,
+    )
+
+    payload = dict(
+        input=dict(
+            model="deepseek-ai/DeepSeek-R1-Distill-Llama-8B",
+            messages=[
+                {"role": "user", "content": "The capital of France is"}
+            ],
+            max_tokens=250,
+            temperature=0
+        ),
+        endpoint="/v1/chat/completions",
+        method="POST"
+    )
+
     req_data = dict(payload=payload, auth_data=auth_data)
     url = urljoin(url, WORKER_ENDPOINT)
     print(f"url: {url}")
@@ -103,12 +159,14 @@ if __name__ == "__main__":
     )
     if endpoint_api_key:
         try:
-            call_generate(
+            print("Calling /generic with target /v1/completions")
+            call_generic_completions(
                 api_key=endpoint_api_key,
                 endpoint_group_name=args.endpoint_group_name,
                 server_url=args.server_url,
             )
-            call_generate_stream(
+            print("Calling /generic with target /v1/chat/completions")
+            call_generic_chat_completions(
                 api_key=endpoint_api_key,
                 endpoint_group_name=args.endpoint_group_name,
                 server_url=args.server_url,

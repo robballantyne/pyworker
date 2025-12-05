895cad15eb7bb243b357fc25924df647f8afce13
Rob Ballantyne <rob@vast.ai>
2025-12-05 13:43:31 +0000

    Fix default pyworker path

--- start_server.sh
+++ start_server.sh
@@ -65,7 +65,7 @@ then
     fi
 
     # Fork testing
-    [[ ! -d $SERVER_DIR ]] && git clone "${PYWORKER_REPO:-https://github.com/robballantyne/vespa}" "$SERVER_DIR"
+    [[ ! -d $SERVER_DIR ]] && git clone "${PYWORKER_REPO:-https://github.com/vast-ai/pyworker}" "$SERVER_DIR"
     if [[ -n ${PYWORKER_REF:-} ]]; then
         (cd "$SERVER_DIR" && git checkout "$PYWORKER_REF")
     fi
